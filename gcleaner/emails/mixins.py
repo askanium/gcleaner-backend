a73071bd1f5a7b74d6758300ccf604cba4352a28
Iulian Gulea <iulian.gulea@gmail.com>
2019-04-23 20:29:45 +0300

    Improve exception handling to cover google error related to token refresh.

--- gcleaner/emails/mixins.py
+++ gcleaner/emails/mixins.py
@@ -1,5 +1,40 @@
+from django.db import connection, transaction
+
+from google.auth.exceptions import RefreshError
+from rest_framework import status
+from rest_framework.views import exception_handler
+from rest_framework.response import Response
+
 from gcleaner.emails.services import EmailService
 from gcleaner.utils.mixins import APIJWTDecoderMixin
+
+
+def set_rollback():
+    atomic_requests = connection.settings_dict.get('ATOMIC_REQUESTS', False)
+    if atomic_requests and connection.in_atomic_block:
+        transaction.set_rollback(True)
+
+
+def emails_exception_handler(exc, context):
+    """
+    Returns the response that should be used for any given exception.
+
+    Additionally to standard rest framework exception handling, it handles
+    RefreshError from GMail API that indicates the access token needs to
+    be refreshed.
+
+    Any unhandled exceptions may return `None`, which will cause a 500 error
+    to be raised.
+    """
+    if isinstance(exc, RefreshError):
+        headers = {'X-Reason': 'Token Expired'}
+        data = {'detail': 'token_expired'}
+
+        set_rollback()
+
+        return Response(data, status=status.HTTP_407_PROXY_AUTHENTICATION_REQUIRED, headers=headers)
+    else:
+        return exception_handler(exc, context)
 
 
 class EmailMixin(APIJWTDecoderMixin):
@@ -15,3 +50,11 @@ class EmailMixin(APIJWTDecoderMixin):
         service = self.service_class(self.get_google_credentials(self.request), self.request.user)
 
         return service
+
+    def get_exception_handler(self):
+        """
+        Return the augmented standard exception handler with GMail API related
+        error handling.
+        :return: The exception handler function.
+        """
+        return emails_exception_handler
--- gcleaner/tests/test_mixins.py
+++ gcleaner/tests/test_mixins.py
@@ -1,9 +1,13 @@
 from django.conf import settings
+from google.auth.exceptions import RefreshError
 
 from google.oauth2.credentials import Credentials
+from rest_framework.response import Response
 from rest_framework.test import APIRequestFactory
 from rest_framework_jwt.utils import jwt_payload_handler, jwt_encode_handler
 
+from gcleaner.emails.mixins import EmailMixin, emails_exception_handler
+from gcleaner.emails.services import EmailService
 from gcleaner.users.models import User
 from gcleaner.utils.mixins import APIJWTDecoderMixin
 
@@ -40,3 +44,49 @@ def test_api_jwt_decoder_mixin_returns_no_credentials_if_no_authorization_header
     credentials = instance.get_google_credentials(request)
 
     assert credentials is None
+
+
+def test_email_mixin_get_service_returns_email_service(mocker, google_credentials, user):
+    # test setup and mocking
+    request = mocker.Mock()
+    request.user = user
+    mixin = EmailMixin()
+    mixin.get_google_credentials = mocker.Mock()
+    mixin.request = request
+    mixin.get_google_credentials.return_value = google_credentials
+
+    # method call
+    service = mixin.get_service()
+
+    # assertions
+    assert isinstance(service, EmailService)
+    mixin.get_google_credentials.assert_called_once_with(request)
+
+
+def test_email_mixin_get_exception_handler_returns_improved_handler():
+    # test setup and mocking
+    mixin = EmailMixin()
+
+    # method call
+    exc_handler = mixin.get_exception_handler()
+
+    # assertions
+    assert exc_handler == emails_exception_handler
+
+
+def test_exception_handler_handles_google_token_expired_error():
+    # test setup and mocking
+    error_dict = {
+        "error": "invalid_grant",
+        "error_description": "Token has been expired or revoked."
+    }
+    exception = RefreshError('invalid_grant: Token has been expired or revoked.', error_dict)
+
+    # function call
+    response = emails_exception_handler(exception, {})
+
+    # assertions
+    assert isinstance(response, Response)
+    assert response.data == {'detail': 'token_expired'}
+    assert response.has_header('X-Reason')
+    assert response.status_code == 407
